6d8dd16191cc7d00de61d4f99f6df86f762c63c6
Rafael Avelar <rafaelksd@live.com>
2022-02-21 21:54:20 -0600

    Escribe las pEscribe para los casos del ejercicio 3

--- adventod2015/test/adventod2015_test.exs
+++ adventod2015/test/adventod2015_test.exs
@@ -2,7 +2,15 @@ defmodule Adventod2015Test do
   use ExUnit.Case
   doctest Adventod2015
 
-  test "greets the world" do
+  test "> delivers presents to 2 houses" do
+    assert Adventod2015.hello() == :world
+  end
+
+  test "^>v< delivers presents to 4 houses in a square" do
+    assert Adventod2015.hello() == :world
+  end
+
+  test "^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses" do
     assert Adventod2015.hello() == :world
   end
 end
