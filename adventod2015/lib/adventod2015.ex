e6d8272d6b562977dbc433252dc8f5326e65c019
Rafael Avelar <rafaelksd@live.com>
2022-02-21 21:53:25 -0600

    Crea proyecto advent of code 2015

---
+++ adventod2015/lib/adventod2015.ex
@@ -0,0 +1,18 @@
+defmodule Adventod2015 do
+  @moduledoc """
+  Documentation for `Adventod2015`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adventod2015.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ adventod2015/test/adventod2015_test.exs
@@ -0,0 +1,8 @@
+defmodule Adventod2015Test do
+  use ExUnit.Case
+  doctest Adventod2015
+
+  test "greets the world" do
+    assert Adventod2015.hello() == :world
+  end
+end
