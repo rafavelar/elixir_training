d4b41b5926858ef5a867d88c5365c49dd3b8242d
Rafael Avelar <rafaelksd@live.com>
2022-02-21 22:14:18 -0600

    Corrige la igualación y la cambia por comparación en los test

--- adventod2015/test/adventod2015_test.exs
+++ adventod2015/test/adventod2015_test.exs
@@ -3,14 +3,14 @@ defmodule Adventod2015Test do
   doctest Adventod2015
 
   test "> delivers presents to 2 houses" do
-    assert Adventod2015.deliver(">") = 2
+    assert Adventod2015.deliver(">") == 2
   end
 
   test "^>v< delivers presents to 4 houses in a square" do
-    assert Adventod2015.deliver("^>v<") = 4
+    assert Adventod2015.deliver("^>v<") == 4
   end
 
   test "^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses" do
-    assert Adventod2015.deliver("^v^v^v^v^v") = 2
+    assert Adventod2015.deliver("^v^v^v^v^v") == 2
   end
 end
