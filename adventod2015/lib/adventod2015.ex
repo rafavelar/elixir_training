1e5be45b1b974286eff56f2a8501a615bffe813c
Rafael Avelar <rafaelksd@live.com>
2022-02-21 23:06:24 -0600

    Elimina la documentación del modulo ya que daba el error: '1 doctest, 3 tests, 1 failure'

--- adventod2015/lib/adventod2015.ex
+++ adventod2015/lib/adventod2015.ex
@@ -1,17 +1,4 @@
 defmodule Adventod2015 do
-  @moduledoc """
-  Documentation for `Adventod2015`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Adventod2015.hello()
-      :world
-
-  """
   def deliver(path) do
     path
     |> String.to_charlist()
